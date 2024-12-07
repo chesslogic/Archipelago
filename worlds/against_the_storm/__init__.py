ed6ca15b0c1afd49da00ce0dff555551cc4a200d
chesslogic <chesslogicmaster@gmail.com>
2024-12-07 09:01:16 -0800

    fmt (I am willing to change my formatter config, I just want to stop seeing warnings)

--- worlds/against_the_storm/__init__.py
+++ worlds/against_the_storm/__init__.py
@@ -11,6 +11,7 @@ from .Locations import ATSLocationClassification, AgainstTheStormLocation, locat
 from .Options import AgainstTheStormOptions, RecipeShuffle
 from .Recipes import satisfies_recipe, blueprint_recipes, nonitem_blueprint_recipes
 
+
 class AgainstTheStormWorld(World):
     """
     Against the Storm is a roguelite city builder
@@ -29,7 +30,7 @@ class AgainstTheStormWorld(World):
         self.included_location_indices: list[int] = []
         self.production_recipes: Dict[str, List[List]] = {}
         self.filler_items: List[str] = []
-    
+
     def are_recipes_beatable(self, production_recipes: Dict[str, List[List]]):
         glade_blueprints = [bp for bp in nonitem_blueprint_recipes if
                             bp != "Crude Workstation" and bp != "Field Kitchen" and bp != "Makeshift Post"]
@@ -49,12 +50,15 @@ class AgainstTheStormWorld(World):
                         break
                 if not satisfied:
                     return False
-        
+
         return True
 
     def generate_early(self):
-        base_locations = [name for (name, (classification, _logic)) in location_dict.items() if classification == ATSLocationClassification.basic or classification == ATSLocationClassification.dlc and self.options.enable_dlc]
-        total_location_count = len(base_locations) + self.options.reputation_locations_per_biome.value * (8 if self.options.enable_dlc else 6) + self.options.extra_trade_locations.value + (self.options.grove_expedition_locations if self.options.enable_dlc else 0)
+        base_locations = [name for (name, (classification, _logic)) in location_dict.items() if
+                          classification == ATSLocationClassification.basic or classification == ATSLocationClassification.dlc and self.options.enable_dlc]
+        total_location_count = len(base_locations) + self.options.reputation_locations_per_biome.value * (
+            8 if self.options.enable_dlc else 6) + self.options.extra_trade_locations.value + (
+                                   self.options.grove_expedition_locations if self.options.enable_dlc else 0)
         total_item_count = len([name for (name, (_class, classification)) in item_dict.items() if
                                 classification == ATSItemClassification.good or
                                 classification == ATSItemClassification.guardian_part and self.options.seal_items or
@@ -64,8 +68,9 @@ class AgainstTheStormWorld(World):
             while total_location_count < total_item_count:
                 self.options.reputation_locations_per_biome.value += 1
                 total_location_count += 8 if self.options.enable_dlc else 6
-            logging.warning(f"[Against the Storm] Fewer locations than items detected in options, increased reputation_locations_per_biome to {self.options.reputation_locations_per_biome.value} to fit all items")
-        
+            logging.warning(
+                f"[Against the Storm] Fewer locations than items detected in options, increased reputation_locations_per_biome to {self.options.reputation_locations_per_biome.value} to fit all items")
+
         self.included_location_indices.append(1)
         # This evenly spreads the option's number of locations between 2 and 17
         # Generating, for example, [10], [4, 8, 11, 15], or [2-17 sans 9]
@@ -79,10 +84,10 @@ class AgainstTheStormWorld(World):
         all_production.update(nonitem_blueprint_recipes)
         if self.options.recipe_shuffle.value != "vanilla":
             skipCWS = self.options.recipe_shuffle.value == RecipeShuffle.option_exclude_crude_ws or \
-                self.options.recipe_shuffle.value == RecipeShuffle.option_exclude_crude_ws_and_ms_post
+                      self.options.recipe_shuffle.value == RecipeShuffle.option_exclude_crude_ws_and_ms_post
             skipMSP = self.options.recipe_shuffle.value == RecipeShuffle.option_exclude_ms_post or \
-                self.options.recipe_shuffle.value == RecipeShuffle.option_exclude_crude_ws_and_ms_post
-            while True: # Break at the bottom when `are_recipes_beatable`
+                      self.options.recipe_shuffle.value == RecipeShuffle.option_exclude_crude_ws_and_ms_post
+            while True:  # Break at the bottom when `are_recipes_beatable`
                 all_recipes: List[Tuple[str, int]] = []
                 for blueprint, recipes in all_production.items():
                     if blueprint == "Crude Workstation" and skipCWS or blueprint == "Makeshift Post" and skipMSP:
@@ -102,12 +107,14 @@ class AgainstTheStormWorld(World):
                 if self.are_recipes_beatable(self.production_recipes):
                     break
         else:
-            self.production_recipes = { key:[[item, num] for item,num in value.items()] for key,value in all_production.items() if not isinstance(value, str) }
-            
+            self.production_recipes = {key: [[item, num] for item, num in value.items()] for key, value in
+                                       all_production.items() if not isinstance(value, str)}
+
     def get_filler_item_name(self):
         choice = self.multiworld.random.choices(self.filler_items)[0]
         # Reroll Survivor Bonding to half its occurence
-        return self.multiworld.random.choices(self.filler_items)[0] if choice == "Survivor Bonding" and self.multiworld.random.random() < 0.5 else choice
+        return self.multiworld.random.choices(self.filler_items)[
+            0] if choice == "Survivor Bonding" and self.multiworld.random.random() < 0.5 else choice
 
     def create_item(self, item: str) -> AgainstTheStormItem:
         return AgainstTheStormItem(item, item_dict.get(item)[0], self.item_name_to_id[item], self.player)
@@ -128,19 +135,19 @@ class AgainstTheStormWorld(World):
             if ATSItemClassification.dlc_blueprint == classification:
                 if self.options.enable_dlc and self.options.blueprint_items:
                     itempool.append(item_key)
-        
+
         # Fill remaining itempool space with filler
         while len(itempool) < len(self.multiworld.get_unfilled_locations(self.player)):
             itempool += [self.create_filler().name]
-        
+
         self.multiworld.itempool += map(self.create_item, itempool)
 
     def create_regions(self) -> None:
         location_pool: Dict[str, int] = {}
-        
+
         menu_region = Region("Menu", self.player, self.multiworld)
         self.multiworld.regions.append(menu_region)
-        
+
         trade_locations = []
         for name, (classification, logic) in location_dict.items():
             if ATSLocationClassification.basic == classification:
@@ -164,7 +171,7 @@ class AgainstTheStormWorld(World):
                     expedition_index = int(re.search(r"^Coastal Grove - (\d\d?)\w\w Expedition$", name).group(1))
                     if expedition_index <= self.options.grove_expedition_locations:
                         location_pool[name] = self.location_name_to_id[name]
-        
+
         trade_locations = sample(trade_locations, self.options.extra_trade_locations.value)
         for location in trade_locations:
             location_pool[location] = self.location_name_to_id[location]
@@ -175,23 +182,31 @@ class AgainstTheStormWorld(World):
         self.multiworld.regions.append(main_region)
 
         menu_region.connect(main_region)
-    
+
     def can_goal(self, state: CollectionState) -> bool:
-        if self.options.seal_items and not state.has_all(["Guardian Heart", "Guardian Blood", "Guardian Feathers", "Guardian Essence"], self.player):
+        if self.options.seal_items and not state.has_all(
+                ["Guardian Heart", "Guardian Blood", "Guardian Feathers", "Guardian Essence"], self.player):
             return False
-        
+
         if self.options.required_seal_tasks.value > 1:
-            return satisfies_recipe(state, self.player, self.production_recipes if self.options.blueprint_items.value else None,
-                ['Jerky,Porridge,Skewers,Biscuits,Pie,Pickled Goods,Paste', 'Ale,Training Gear,Incense,Scrolls,Wine,Tea',
-                 'Coal,Oil,Sea Marrow', 'Amber', 'Tools', 'Purging Fire', 'Planks', 'Bricks', 'Fabric',
-                 # Above is the baseline that ensures normal winnable conditions, below ensures every Seal task
-                 'Pack of Crops', 'Pack of Provisions', 'Pack of Building Materials', 'Stone,Sea Marrow,Training Gear',
-                 'Pipes', 'Parts', 'Ancient Tablet'])
+            return satisfies_recipe(state, self.player,
+                                    self.production_recipes if self.options.blueprint_items.value else None,
+                                    ['Jerky,Porridge,Skewers,Biscuits,Pie,Pickled Goods,Paste',
+                                     'Ale,Training Gear,Incense,Scrolls,Wine,Tea',
+                                     'Coal,Oil,Sea Marrow', 'Amber', 'Tools', 'Purging Fire', 'Planks', 'Bricks',
+                                     'Fabric',
+                                     # Above is the baseline that ensures normal winnable conditions, below ensures every Seal task
+                                     'Pack of Crops', 'Pack of Provisions', 'Pack of Building Materials',
+                                     'Stone,Sea Marrow,Training Gear',
+                                     'Pipes', 'Parts', 'Ancient Tablet'])
         else:
-            return satisfies_recipe(state, self.player, self.production_recipes if self.options.blueprint_items.value else None,
-                ['Jerky,Porridge,Skewers,Biscuits,Pie,Pickled Goods,Paste', 'Ale,Training Gear,Incense,Scrolls,Wine,Tea',
-                 'Coal,Oil,Sea Marrow', 'Amber', 'Tools', 'Purging Fire', 'Planks', 'Bricks', 'Fabric'])
-    
+            return satisfies_recipe(state, self.player,
+                                    self.production_recipes if self.options.blueprint_items.value else None,
+                                    ['Jerky,Porridge,Skewers,Biscuits,Pie,Pickled Goods,Paste',
+                                     'Ale,Training Gear,Incense,Scrolls,Wine,Tea',
+                                     'Coal,Oil,Sea Marrow', 'Amber', 'Tools', 'Purging Fire', 'Planks', 'Bricks',
+                                     'Fabric'])
+
     def check_other_location_rules(self, location: str, state: CollectionState, player: int):
         if location == "The Marshlands - Harvest from an Ancient Proto Wheat":
             return state.has("Forager's Camp", player)
@@ -199,19 +214,19 @@ class AgainstTheStormWorld(World):
             return state.has("Trapper's Camp", player)
         elif location == "The Marshlands - Harvest from a Giant Proto Fungus":
             return state.has("Herbalist's Camp", player)
-        
+
         return True
-    
+
     def set_rules(self) -> None:
         self.multiworld.completion_condition[self.player] = lambda state: self.can_goal(state)
         for location in self.multiworld.get_locations(self.player):
             logic = location_dict[location.name][1]
             set_rule(location,
                      lambda state, l=logic: self.check_other_location_rules(location.name, state, self.player) and \
-                        satisfies_recipe(
-                            state, self.player,
-                            self.production_recipes if self.options.blueprint_items.value else None,
-                            l)
+                                            satisfies_recipe(
+                                                state, self.player,
+                                                self.production_recipes if self.options.blueprint_items.value else None,
+                                                l)
                      )
 
     def fill_slot_data(self) -> Dict[str, Any]:
@@ -226,4 +241,3 @@ class AgainstTheStormWorld(World):
             "rep_location_indices": self.included_location_indices,
             "production_recipes": self.production_recipes
         }
-    
